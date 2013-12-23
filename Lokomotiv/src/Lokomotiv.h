607e700bffb3a560c2d65cfdae546028e48c83f3
blacksound <blekesaune@gmail.com>
2013-12-23 13:04:00 +0100

    Added outline structure

---
+++ Lokomotiv/src/Lokomotiv.h
@@ -0,0 +1,14 @@
+#ifndef LOKOMOTIV_H
+#define LOKOMOTIV_H
+#include "VNH5019Driver.h"
+#include "IRReader.h"
+#include "HallEncoder.h"
+#include "LEDDimmer.h"
+#include "LokomotivMotor.h"
+
+class Lokomotiv{
+	Lokomotiv() {};
+	~Lokomotiv();
+}
+
+#endif
---
+++ Lokomotiv/src/sketch.ino
@@ -0,0 +1,11 @@
+#include "Lokomotiv.h"
+#include "LokomotivParser.h"
+//#include "LokomotivAPI.h"
+
+void setup()
+{
+}
+
+void loop()
+{
+}
